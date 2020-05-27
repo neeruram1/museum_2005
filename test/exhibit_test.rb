9d4c4c16c76e51b0904622161ff53099a8804792
“Neeru <neeram85@gmail.com>
2020-05-27 09:06:59 -0600

    Test Exhibit class exists

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -1,7 +1,14 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require '.lib/exhibit'
+require './lib/exhibit'
 require 'pry'
 
-class ExhibitTest < Minitest::Test 
+class ExhibitTest < Minitest::Test
+  def setup
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  def test_it_exists
+    assert_instance_of Exhibit, @exhibit
+  end
 end
