abf7942b3ea4919111fe62b9369512f07232cab4
“Neeru <neeram85@gmail.com>
2020-05-27 10:01:04 -0600

    Test it can start with no patrons

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -8,14 +8,21 @@ require './lib/exhibit'
 class MuseumTest < Minitest::Test
   def setup
     @dmns = Museum.new("Denver Museum of Nature and Science")
+
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
+
     @patron_1 = Patron.new("Bob", 20)
     @patron_2 = Patron.new("Sally", 20)
+    @patron_3 = Patron.new("Johnny", 5)
+
     @patron_1.add_interest("Gems and Minerals")
     @patron_1.add_interest("Dead Sea Scrolls")
+
     @patron_2.add_interest("IMAX")
+
+    @patron_3.add_interest("Dead Sea Scrolls")
   end
 
   def test_it_exists
@@ -46,4 +53,10 @@ class MuseumTest < Minitest::Test
     assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@patron_1)
     assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
   end
+
+  def test_it_starts_with_no_patrons
+    assert_equal [], @dmns.patrons
+  end
+
+
 end
