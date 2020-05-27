ce8fa45c28666f651b90e3598635bcbdde60d1a5
“Neeru <neeram85@gmail.com>
2020-05-27 09:25:58 -0600

    Create Museum class test file and initial tests

---
+++ test/museum_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/museum'
+require './lib/patron'
+require './lib/exhibit'
+
+class MuseumTest < Minitest::Test
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
+  end
+
+  def test_it_exists
+    assert_instance_of Museum, @dmns
+  end
+
+  def test_it_has_attributes
+    assert_equal "Denver Museum of Nature and Science", @dmns.name
+  end
+end
